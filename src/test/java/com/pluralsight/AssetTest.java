38fb3a4c7358a11f9d6b83abf44bf45e09848a14
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-05-14 14:19:41 -0500

    Add JUnit test for getValue()

--- src/test/java/com/pluralsight/AssetTest.java
+++ src/test/java/com/pluralsight/AssetTest.java
@@ -18,4 +18,30 @@ class AssetTest {
         //assert
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void this_test_should_return_description() {
+        //arrange
+        Cash cash = new Cash("Emergency Fund: ", "2024-01-01", 500.00);
+
+        //act
+        String actual = cash.getDescription();
+        String expected = "Emergency Fund: ";
+
+        //assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void this_test_should_return_value() {
+        //arrange
+        House house = new House("Hobbit Hole / Smial", "April 12, 2841", 450.000, "Bag End, Underhill, Hobbiton", 1, 1250, 1575);
+
+        //act
+        double actual = house.getValue();
+        double expected = (180 * 1250) + (0.25 * 1575);
+
+        //assert
+        assertEquals(expected, actual);
+    }
 }
